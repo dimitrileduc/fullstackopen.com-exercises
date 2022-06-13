42fce4019eb80ac2ff78fd4b13fd8613b35047c8
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-06-13 21:48:08 +0200

    1.2

--- part1/part1-ex/src/components/Content/index.jsx
+++ part1/part1-ex/src/components/Content/index.jsx
@@ -1,15 +1,13 @@
 import React from "react";
+import Part from "./Part";
 
 function Content({part1, part2, part3, exercises1, exercises2, exercises3}) {
     return (
         <>
             <h2>Content</h2>
-            <h3>{part1}</h3>
-            <p>{exercises1}</p>
-            <h3>{part2}</h3>
-            <p>{exercises2}</p>
-            <h3>{part3}</h3>
-            <p>{exercises3}</p>
+            <Part partName={part1} number={exercises1} />
+            <Part partName={part2} number={exercises2} />
+            <Part partName={part3} number={exercises3} />
         </>
     );
 }
