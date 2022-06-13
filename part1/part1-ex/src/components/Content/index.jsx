10c52de65fd2adefae75cf16128ba48de5649377
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-06-13 22:20:48 +0200

    1.4

--- part1/part1-ex/src/components/Content/index.jsx
+++ part1/part1-ex/src/components/Content/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Part from "./Part";
 
-function Content({part1, part2, part3, exercises1, exercises2, exercises3}) {
+function Content({parts}) {
+    const arrayNames = parts.map((value) => value.name);
+    const arrayNumber = parts.map((value) => value.exercises);
     return (
         <>
             <h2>Content</h2>
-            <Part partName={part1} number={exercises1} />
-            <Part partName={part2} number={exercises2} />
-            <Part partName={part3} number={exercises3} />
+            <Part partName={arrayNames[0]} number={arrayNumber[0]} />
+            <Part partName={arrayNames[1]} number={arrayNumber[1]} />
+            <Part partName={arrayNames[2]} number={arrayNumber[2]} />
         </>
     );
 }
--- part1/part1-ex/src/components/Total/index.jsx
+++ part1/part1-ex/src/components/Total/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-function Total({totalex}) {
-    return <h2>total :{totalex}</h2>;
+function Total({parts}) {
+    const arrayNumber = parts.map((value) => value.exercises);
+    const total = arrayNumber[0] + arrayNumber[1] + arrayNumber[2];
+
+    return <h2>total :{total}</h2>;
 }
 
 export default Total;
