2c517af54919b0e5db56987366d718d35091d7f2
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-06-17 12:12:41 +0200

    2.8

--- part2/phonebook-ex/src/App.js
+++ part2/phonebook-ex/src/App.js
@@ -3,6 +3,7 @@ import {useState} from "react";
 const App = () => {
     const [persons, setPersons] = useState([{name: "Arto Hellas", id: 1}]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
     let objectExist = false;
 
     const addPersons = (event) => {
@@ -23,6 +24,7 @@ const App = () => {
         if (!objectExist) {
             const personObject = {
                 name: newName,
+                number: newNumber,
                 id: persons.length + 1,
             };
 
@@ -36,6 +38,11 @@ const App = () => {
         setNewName(event.target.value);
     };
 
+    const handleNumberChange = (event) => {
+        console.log(event.target.value);
+        setNewNumber(event.target.value);
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
@@ -43,6 +50,16 @@ const App = () => {
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
                 </div>
+                <label htmlFor="phone">Enter a phone number:</label>
+                <input
+                    type="tel"
+                    id="phone"
+                    name="phone"
+                    placeholder="123-1234567"
+                    pattern="[0-9]{3}-[0-9]{7}"
+                    required
+                    onChange={handleNumberChange}></input>
+                <small>Format: 123-4567898</small>
                 <div>
                     <button type="submit">add</button>
                 </div>
@@ -51,7 +68,10 @@ const App = () => {
             <h2>Numbers</h2>
             <ul>
                 {persons.map((person) => (
-                    <li key={person.id}> {person.name}</li>
+                    <li key={person.id}>
+                        {" "}
+                        {person.name} number : {person.number}
+                    </li>
                 ))}
             </ul>
             ...
